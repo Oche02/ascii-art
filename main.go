7a8a0a32d0ed9360ab8c4b1499059d7433b012c7
Augustine Oche <augustineoche02@gmail.com>
2026-05-05 11:02:38 +0000

    updated main.go file to handle an empty string case

--- main.go
+++ main.go
@@ -3,20 +3,18 @@ package main
 import(
 	"os"
 	"fmt"
-	"time"
 )
 
 func main(){
-	for i := 0; i <= 100; i++ {
-		fmt.Printf("\rLoading...%d%%", i)
-		time.Sleep(10 * time.Millisecond)
-	}
 	if len(os.Args) != 2{
 		fmt.Println("ATTENTION: expecting >> go run . [string]")
 		os.Exit(1)
 	}
+	input := os.Args[1]
+	if input == ""{
+		os.Exit(0)
+	}
 
-	// input := os.Args[1]
 
 	// data, err:= os.ReadFile("banner.txt")
 	// if err != nil{
