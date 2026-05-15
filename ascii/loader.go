649a50ca1b583215bd716cab11ff39d1cf47504c
Augustine Oche <augustineoche02@gmail.com>
2026-05-15 15:35:24 +0000

    i modified LoadBanner function to be able to read banner file

--- ascii/loader.go
+++ ascii/loader.go
@@ -1,1 +1,14 @@
 package ascii
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func LoadBanner(filePath string)(map[rune][]string){
+	data, err := os.ReadFile(filePath)
+	if err != nil{
+		fmt.Fprintf(os.Stderr, "failed to read banner file %v\n", err)
+	}
+}
